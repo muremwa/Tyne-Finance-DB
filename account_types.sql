1bbdfd0bc3e5999ccc92c0cae8b6228e1830d266
Daniel Muremwa Mburu <muremwa@users.noreply.github.com>
2023-05-23 12:36:12 +0300

    add account types

---
+++ account_types.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    core_accounttype (name, code)
+VALUES
+    ('Mobile Money', 'MM001'),
+    ('Card', 'CR002'),
+    ('Digital Wallet', 'DW003'),
+    ('Bank', 'BK004'),
+    ('Cash', 'CS005');
